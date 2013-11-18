598eff131425971ac09342f436900ed3a5f815b0
matchs <contato@mateuchagas.com.br>
2013-11-18 18:44:49 -0300

    Parser rules tests

---
+++ parser_rules/rte.js
@@ -0,0 +1,603 @@
+var wysihtml5ParserRules = {
+  // More info about the parser rules in `wysihtml5_parser_sample.js` file.
+  "classes": {
+    "period-divider": 1
+  },
+  "tags": {
+    "a": {
+      "check_attributes": {
+        "href": "href"
+      },
+      "set_attributes": {
+        "rel": "nofollow",
+        "target": "_blank"
+      },
+      "parse":function(text){//When dealing with links, remove any whitespace and also put everything to lowercase
+        return text.toLowerCase().replace(/(?!^)\s+(?!$)/gi,'');
+      }
+    },
+    "abbr": {
+      "rename_tag": "span"
+    },
+    "acronym": {
+      "rename_tag": "span"
+    },
+    "address": {
+      "rename_tag": "div"
+    },
+    "applet": {
+      "remove": 1
+    },
+    "area": {
+      "remove": 1
+    },
+    "b": {},
+    "base": {
+      "remove": 1
+    },
+    "basefont": {
+      "remove": 1
+    },
+    "bdi": {
+      "rename_tag": "span"
+    },
+    "bdo": {
+      "rename_tag": "span"
+    },
+    "bgsound": {
+      "remove": 1
+    },
+    "big": {
+      "rename_tag": "span",
+      "set_class": "wysiwyg-font-size-larger"
+    },
+    "blockquote": {},
+    "blink": {
+      "rename_tag": "span"
+    },
+    "body": {
+      "rename_tag": "div"
+    },
+    "br": {
+      "remove": 1
+    },
+    "button": {
+      "rename_tag": "span"
+    },
+    "code": {},
+    "command": {
+      "remove": 1
+    },
+    "caption": {
+      "add_class": {
+        "align": "align_text"
+      }
+    },
+    "canvas": {
+      "remove": 1
+    },
+    "center": {
+      "rename_tag": "div",
+      "set_class": "wysiwyg-text-align-center"
+    },
+    "col": {
+      "remove": 1
+    },
+    "cite": {},
+    "colgroup": {
+      "remove": 1
+    },
+    "comment": {
+      "remove": 1
+    },
+    "details": {
+      "rename_tag": "div"
+    },
+    "dir": {
+      "rename_tag": "ul"
+    },
+    "div": {
+      "rename_tag": "p"
+    },
+    "em": {},
+    "tr": {
+      "rename_tag": "p"
+    },
+    "strike": {
+      "remove": 1
+    },
+    "form": {
+      "rename_tag": "div"
+    },
+    "rt": {
+      "rename_tag": "span"
+    },
+    "title": {
+      "remove": 1
+    },
+    "multicol": {
+      "rename_tag": "div"
+    },
+    "figure": {
+      "rename_tag": "div"
+    },
+    "xmp": {
+      "rename_tag": "span"
+    },
+    "small": {
+      "rename_tag": "span",
+      "set_class": "wysiwyg-font-size-smaller"
+    },
+    "time": {
+      "rename_tag": "span"
+    },
+    "ul": {},
+    "progress": {
+      "rename_tag": "span"
+    },
+    "dfn": {
+      "rename_tag": "span"
+    },
+    "iframe": {
+      "check_attributes":{
+        "src":"url",
+        "width":"numbers",
+        "height":"numbers"
+      }
+    },
+    "figcaption": {
+      "rename_tag": "div"
+    },
+    "img": {
+      "check_attributes": {
+        "width": "numbers",
+        "alt": "alt",
+        "src": "src",
+        "height": "numbers"
+      },
+      "add_class": {
+        "align": "align_img"
+      }
+    },
+    "rb": {
+      "rename_tag": "span"
+    },
+    "footer": {
+      "rename_tag": "div"
+    },
+    "noframes": {
+      "remove": 1
+    },
+    "u": {
+      "rename_tag": "span"
+    },
+    "sup": {
+      "rename_tag": "span"
+    },
+    "nav": {
+      "rename_tag": "div"
+    },
+    "h1": {
+      "rename_tag": "h3"
+    },
+    "h2": {
+      "rename_tag": "h3"
+    },
+    "h3": {},
+    "h4": {
+      "rename_tag": "h3"
+    },
+    "h5": {
+      "rename_tag": "h3"
+    },
+    "h6": {
+      "rename_tag": "h3"
+    },
+    "head": {
+      "remove": 1
+    },
+    "tbody": {
+      "rename_tag": "span"
+    },
+    "dd": {
+      "rename_tag": "div"
+    },
+    "s": {
+      "rename_tag": "span"
+    },
+    "li": {},
+    "td": {
+      "rename_tag": "span"
+    },
+    "object": {
+      "remove": 1
+    },
+    "option": {
+      "rename_tag": "span"
+    },
+    "select": {
+      "rename_tag": "span"
+    },
+    "i": {},
+    "track": {
+      "remove": 1
+    },
+    "wbr": {
+      "remove": 1
+    },
+    "fieldset": {
+      "rename_tag": "div"
+    },
+    "noscript": {
+      "remove": 1
+    },
+    "svg": {
+      "remove": 1
+    },
+    "input": {
+      "remove": 1
+    },
+    "table": {
+      "rename_tag": "p"
+    },
+    "keygen": {
+      "remove": 1
+    },
+    "meta": {
+      "remove": 1
+    },
+    "map": {
+      "rename_tag": "div"
+    },
+    "isindex": {
+      "remove": 1
+    },
+    "mark": {
+      "rename_tag": "span"
+    },
+    "tfoot": {
+      "add_class": {
+        "align": "align_text"
+      }
+    },
+    "video": {
+      "remove": 1
+    },
+    "strong": {},
+    "output": {
+      "rename_tag": "span"
+    },
+    "marquee": {
+      "rename_tag": "span"
+    },
+    "q": {
+      "check_attributes": {
+        "cite": "url"
+      }
+    },
+    "span": {},
+    "rp": {
+      "rename_tag": "span"
+    },
+    "spacer": {
+      "remove": 1
+    },
+    "source": {
+      "remove": 1
+    },
+    "aside": {
+      "rename_tag": "div"
+    },
+    "frame": {
+      "remove": 1
+    },
+    "section": {
+      "rename_tag": "div"
+    },
+    "ol": {},
+    "nobr": {
+      "rename_tag": "span"
+    },
+    "html": {
+      "rename_tag": "div"
+    },
+    "summary": {
+      "rename_tag": "span"
+    },
+    "var": {
+      "rename_tag": "span"
+    },
+    "del": {
+      "remove": 1
+    },
+    "style": {
+      "remove": 1
+    },
+    "device": {
+      "remove": 1
+    },
+    "meter": {
+      "rename_tag": "span"
+    },
+    "textarea": {
+      "rename_tag": "span"
+    },
+    "embed": {
+      "remove": 1
+    },
+    "hgroup": {
+      "rename_tag": "div"
+    },
+    "font": {
+      "rename_tag": "span",
+      "add_class": {
+        "size": "size_font"
+      }
+    },
+    "tt": {
+      "rename_tag": "span"
+    },
+    "noembed": {
+      "remove": 1
+    },
+    "thead": {
+      "add_class": {
+        "align": "align_text"
+      }
+    },
+    "plaintext": {
+      "rename_tag": "span"
+    },
+    "xml": {
+      "remove": 1
+    },
+    "param": {
+      "remove": 1
+    },
+    "th": {
+      "rename_tag": "span"
+    },
+    "legend": {
+      "rename_tag": "span"
+    },
+    "hr": {
+      "set_class": "period-divider"
+    },
+    "label": {
+      "rename_tag": "span"
+    },
+    "dl": {
+      "rename_tag": "div"
+    },
+    "kbd": {
+      "rename_tag": "span"
+    },
+    "listing": {
+      "rename_tag": "div"
+    },
+    "dt": {
+      "rename_tag": "span"
+    },
+    "nextid": {
+      "remove": 1
+    },
+    "pre": {},
+    "audio": {
+      "remove": 1
+    },
+    "datalist": {
+      "rename_tag": "span"
+    },
+    "samp": {
+      "rename_tag": "span"
+    },
+    "article": {
+      "rename_tag": "div"
+    },
+    "link": {
+      "remove": 1
+    },
+    "script": {
+      "remove": 1
+    },
+    "menu": {
+      "rename_tag": "ul"
+    },
+    "ins": {
+      "rename_tag": "span"
+    },
+    "p": {
+      "parse":function(txt){
+        return txt.replace(/[^.;:?!]$/g,"$&.")
+          .replace(/^[.][.][.]/,"($&)");
+      }
+    },
+    "frameset": {
+      "remove": 1
+    },
+    "header": {
+      "rename_tag": "div"
+    },
+    "optgroup": {
+      "rename_tag": "span"
+    },
+    "ruby": {
+      "rename_tag": "span"
+    },
+    "sub": {
+      "rename_tag": "span"
+    }
+  },
+  /**
+   * Text parsing patterns
+   *
+   * Used to automatically parse pasted or pre-loaded texts within the editor
+   *
+   *  - rule: A regular expression used to be matched over the whole text that's being inserted within the editor.
+   *  - replace: A string or filtering function. The function receives the matched string and must return a transformation over it.
+   *             The string supports backreferencing for the matched pattern/rule.
+   */
+  "parser": [
+    {
+      "rule": /^\s+|\s+$|\,$/g,
+      "replace": ""
+    },
+    {
+      "rule": /\s{2,}/g,
+      "replace": " "
+    },
+    {
+      "rule": /[.!?][^\s.]/g,
+      "replace": function (txt) {
+        //return txt.replace(txt[0]+txt[1], txt[0]+' '+txt[1].toUpperCase());
+        return txt[0] + ' ' + txt[1].toUpperCase();
+      }
+    },
+    {
+      "rule": /\s\,/g,
+      "replace": ","
+    },
+    {
+      "rule": /[,]\S/g,
+      "replace": function(txt){
+        return txt[0] + ' ' + txt[1]
+      }
+    },
+    {
+      "rule": /!{2,}/g,
+      "replace": "!"
+    },
+    {
+      "rule": /\?{2,}/g,
+      "replace": "?"
+    },
+    {
+      "rule": /([!?](\s+[!?.,:;])+)/g,
+      "replace": function (txt) {
+        return txt[0];
+      }
+    },
+    {
+      "rule": /(!\?|\?!)[!?]+/g,
+      "replace": "!?"
+    },
+    {
+      "rule": /\.{4,}/g,
+      "replace": '...'
+    },
+    {
+      "rule": /[.!?]\s\S/g,
+      "replace": function (txt) {
+        return txt.toUpperCase();
+      }
+    },
+    {
+      "rule": /^./g,
+      "replace": function (txt) {
+        return txt.toUpperCase()
+      }
+    },
+    {
+      "rule": /[@&*%&#+-]{2,}/g,
+      "replace": function(txt){
+        return txt[0];
+      }
+    },
+    {
+      "rule": /[,:;][a-zçáàéèíìóòúùñãõüïâêîôû]/g,
+      "replace": function(txt){
+        return txt[0] + ' ' + txt[1];
+      }
+    },
+    {
+      "rule": /\s+[.,;:!?]/g,
+      "replace": function(txt){
+        return txt[txt.length - 1];
+      }
+    }
+  ],
+
+  /**
+   * Deny patterns
+   *
+   * Used to avoid such patterns to me written within editor
+   *
+   *  - rule: A regular expression to be matched with the future resulting text around the caret position.
+   *  - message: A message to be sent through an event. Can be used to warn the user about mistypes etc.
+   */
+  "deny": [
+    {
+      "rule": /[\u00a0\t\ ]{2,}/ig,
+      "msg": "Avoid using extra spaces"
+    },
+    {
+      "rule": /[\u00a0\t\ ][.,:;!?]/ig,
+      "msg": "Avoid white spaces before punctuation"
+    },
+    {
+      "rule": /[\u00a0\t\ ]+\n/img,
+      "msg": "Avoid empty spaces at the end of line"
+    },
+    {
+      "rule": /(\?{2,}|!{2,}|!\?[!?]+|\?![!?]+)|([!?](\s+[!?.,:;])+)/g,
+      "msg": "Avoid using too much ! or ?"
+    },
+    {
+      "rule": /(\.([,:;!?]+|\.{3,}))|([,:;][.,:;!?]+)|([.,:;](\s+[.,:;!?])+)|([!?]\s*[.,:;])/g,
+      "msg": "Avoid excessive punctuation"
+    },
+    {
+      "rule": /[@&*%&#+-]{2,}/g,
+      "msg": "Avoid excessive usage of symbols"
+    }
+  ],
+
+  /**
+   * Auto-fix or auto-correct patterns
+   *
+   * Used to provide patterns in order to automatically correct them while the text is typed bu the user
+   *
+   *  - rule: A regular expression that matches text around the caret
+   *  - fix:  A function that receives char that's being inputted and returns a transformation over it
+   *
+   */
+  "fix": [
+    {
+      "rule": /[,;]\S/i,//Punctuation followed by any non-whitespace character
+      "fix": function (char) {
+        if (char in [',',';']) {
+          return char + ' '
+        } else {
+          return ' ' + char;
+        }
+      }
+    },
+    {
+      "rule": /:[^\s/]/i,//Inserting white-space after : only if not http:// case
+      "fix": function (char) {
+        if (char == ':') {
+          return char + ' '
+        } else {
+          return ' ' + char;
+        }
+      }
+    },
+    {
+      "rule": /(!|\?)[^!?\s]/i,//Inserting white space after ! or ?
+      "fix": function (char) {
+        if (char in ['!', '?']) {
+          return char + ' '
+        } else {
+          return ' ' + char.toUpperCase();
+        }
+      }
+    },
+    {
+      "rule": /[^\.][\.!?]\s[a-zçáàéèíìóòúùñãõüïâêîôû]/,//Period followed by any non-whitespace character
+      "fix": function (char) {
+        return char.toUpperCase();
+      }
+
+    }
+  ]
+};
---
+++ test/parser_rules/parser_rules_test.js
@@ -0,0 +1,436 @@
+$(function(){
+
+  function ParserRulesTestCase(tagsObject) {
+
+    this.tagsObject = tagsObject;
+    /*
+     * Tests if the object is like the pattern:
+     * "tag": {
+     *   "check_attributes": {
+     *     "attribute": "value"
+     *   }
+     * }
+     */
+    this.assertAttributesValues = function(tag, attributesMap) {
+      var obj = this.tagsObject[tag];
+      for (attribute in attributesMap) {
+        equal(obj["check_attributes"][attribute], attributesMap[attribute], "The tag <" + tag + "> has the attribute '" + attribute + "' with the value '" + attributesMap[attribute] + "' at `check_attributes`.");
+      }
+      equal(Object.keys(obj["check_attributes"]).length, Object.keys(attributesMap).length, "The 'check_attributes' attribute of tag <" + tag + "> key has the expected value.");
+    };
+
+    /*
+     * Tests if the object is like the pattern:
+     * "tag": {
+     *   "set_attributes": {
+     *     "attribute": "value"
+     *   }
+     * }
+     */
+    this.assertSetAttributes = function(tag, attributesMap) {
+      var obj = this.tagsObject[tag];
+      for (attribute in attributesMap) {
+        equal(obj["set_attributes"][attribute], attributesMap[attribute], "The tag <" + tag + "> has the attribute '" + attribute + "' with the value '" + attributesMap[attribute] + "' at `set_attributes`.");
+      }
+      equal(Object.keys(obj["set_attributes"]).length, Object.keys(attributesMap).length, "The 'set_attributes' attribute of tag <" + tag + "> key has the expected value.");
+    };
+
+    /*
+     * Tests if the object is like the pattern:
+     * "tag": {
+     *   "remove": 1
+     * }
+     */
+    this.assertRemoveTag = function(tag) {
+      var obj = this.tagsObject[tag];
+      equal(obj["remove"], 1, "The tag <" + tag + "> will be removed.");
+    };
+
+    /*
+     * Tests if the object is like the pattern:
+     * "tag": {
+     *   "rename_tag": 1
+     * }
+     */
+    this.assertReplaceTag = function(tag, newTag) {
+      var obj = this.tagsObject[tag];
+      try {
+        equal(obj["rename_tag"], newTag, "The tag <" + tag + "> will be replaced by <" + newTag + "> tag.");
+      } catch(e) {
+        if (e instanceof TypeError && obj === undefined) {
+          throw new Error("The tag <" + tag + "> doesn't exists in the tags object");
+        }
+        else {
+          throw e
+        }
+      }
+    };
+
+    /*
+     * Tests if the object is like the pattern:
+     * "tag": {
+     *   "set_class": "class"
+     * }
+     */
+    this.assertSetClass = function(tag, htmlClass) {
+      var obj = this.tagsObject[tag];
+      equal(obj["set_class"], htmlClass, "The 'set_class' attribute has the class" + htmlClass);
+    };
+
+    /*
+     * Tests if the object is like the pattern:
+     * "tag": {
+     *   "add_class": "class"
+     * }
+     */
+    this.assertAddClass = function(tag, classMap) {
+      var obj = this.tagsObject[tag];
+      for (key in classMap) {
+        equal(obj["add_class"][key], classMap[key], "The tag <" + tag + "> has the class '" + key + "' with the value '" + classMap[key] + "' at `add_class` map.");
+      }
+      equal(Object.keys(obj["add_class"]).length, Object.keys(classMap).length, "The 'add_class' attribute of tag <" + tag + "> key has the expected value.");
+    };
+
+    /*
+     * Tests if the object is like the pattern:
+     * "tag": {}
+     */
+    this.assertEmptyRule = function(tag) {
+      var obj = this.tagsObject[tag];
+      deepEqual(obj, {}, "The tag <" + tag + "> hasn't any parser rule.");
+    };
+
+
+    /**
+     * Tests if a given rule matches an inputted text correctly
+     * @param rule
+     * @param inputSet Array
+     */
+    this.assertDenyRule = function(rule, inputSet){
+      for(var i in inputSet){
+        equal(rule.rule.test(inputSet[i]), true, 'text '+inputSet[i]+' was denied by rule '+rule.rule);
+      }
+    }
+
+    /**
+     * Tests if a given rule filters an inputted text correctly
+     * @param rule
+     * @param testSet
+     */
+    this.assertFixRule = function(rule, text, testSet){
+      for(var i in testSet){
+        deepEqual(rule.rule.test(testSet[i].text + testSet[i].input), true, 'Rule matched '+ testSet[i].text+testSet[i].input);
+        deepEqual(testSet[i].text + rule.fix(testSet[i].input), testSet[i].expec, 'Text "'+testSet[i].text + testSet[i].input+'" was transformed into "'+testSet[i].expec + '"');
+      }
+    }
+
+
+    /**
+     * Tests if a given rule parses a given text correctly
+     * @param rule
+     * @param input
+     * @param expec
+     */
+    this.assertParserRules = function(rules, input, expec){
+      var txt = input;
+
+      for(var i in rules){
+        txt = txt.replace(rules[i].rule, rules[i].replace);
+      }
+
+      deepEqual(txt, expec, 'Text: "' + input + "\" was correctly parsed and became:\n\""+txt+'"');
+
+    }
+
+    /**
+     * Tests if a given rule parses the text inside a give node correctly
+     * @param node
+     * @param input
+     * @param expec
+     */
+    this.assertNodeParserRule = function(tags, node, input, expec) {
+      equal(typeof  tags[node], "object", 'the node <' + node + '> has rule');
+      equal(typeof tags[node].parse, "function", 'the node <' + node + '> has specific parser rule' );
+      deepEqual(tags[node].parse(input), expec, 'the node <' + node + '>\'s parser rule parsed "'+ input +'" into "'+ expec +'" correctly');
+    }
+  }
+
+
+  qunitModule('wysihtml5 parser');
+
+  test('Tests the wysihtml5 parser object', function () {
+    //var parseRules = module('editor').parseRules();
+    var parseRules = wysihtml5ParserRules;
+
+    deepEqual(parseRules.classes, {
+      'period-divider': 1,
+    }, 'The allowed classes are as expected');
+
+    var tags = parseRules.tags;
+    var testCase = new ParserRulesTestCase(tags);
+
+    testCase.assertReplaceTag("abbr", "span");
+    testCase.assertReplaceTag("acronym", "span");
+
+    // correct replace anything for div???
+    testCase.assertReplaceTag("address", "div");
+
+    testCase.assertReplaceTag("bdi", "span");
+    testCase.assertReplaceTag("bdo", "span");
+    testCase.assertReplaceTag("big", "span");
+    testCase.assertReplaceTag("blink", "span");
+    testCase.assertReplaceTag("body", "div");
+    //testCase.assertReplaceTag("br", "p");
+    testCase.assertReplaceTag("button", "span");
+    testCase.assertReplaceTag("center", "div");
+    testCase.assertReplaceTag("details", "div");
+    testCase.assertReplaceTag("dir", "ul");
+    testCase.assertReplaceTag("div", "p");
+    testCase.assertReplaceTag("form", "div");
+    testCase.assertReplaceTag("rt", "span");
+    testCase.assertReplaceTag("multicol", "div");
+    testCase.assertReplaceTag("figure", "div");
+    testCase.assertReplaceTag("xmp", "span");
+    testCase.assertReplaceTag("small", "span");
+    testCase.assertReplaceTag("time", "span");
+    testCase.assertReplaceTag("progress", "span");
+    testCase.assertReplaceTag("dfn", "span");
+    testCase.assertReplaceTag("figcaption", "div");
+    testCase.assertReplaceTag("rb", "span");
+    testCase.assertReplaceTag("footer", "div");
+    testCase.assertReplaceTag("u", "span");
+    testCase.assertReplaceTag("sup", "span");
+    testCase.assertReplaceTag("nav", "div");
+    testCase.assertReplaceTag("dd", "div");
+    testCase.assertReplaceTag("s", "span");
+    testCase.assertReplaceTag("option", "span");
+    testCase.assertReplaceTag("select", "span");
+    testCase.assertReplaceTag("map", "div");
+    testCase.assertReplaceTag("mark", "span");
+    testCase.assertReplaceTag("output", "span");
+    testCase.assertReplaceTag("marquee", "span");
+    testCase.assertReplaceTag("rp", "span");
+    testCase.assertReplaceTag("aside", "div");
+    testCase.assertReplaceTag("section", "div");
+    testCase.assertReplaceTag("nobr", "span");
+    testCase.assertReplaceTag("html", "div");
+    testCase.assertReplaceTag("summary", "span");
+    testCase.assertReplaceTag("var", "span");
+    testCase.assertReplaceTag("meter", "span");
+    testCase.assertReplaceTag("textarea", "span");
+    testCase.assertReplaceTag("hgroup", "div");
+    testCase.assertReplaceTag("font", "span");
+    testCase.assertReplaceTag("tt", "span");
+    testCase.assertReplaceTag("plaintext", "span");
+    testCase.assertReplaceTag("legend", "span");
+    testCase.assertReplaceTag("label", "span");
+    testCase.assertReplaceTag("dl", "div");
+    testCase.assertReplaceTag("kbd", "span");
+    testCase.assertReplaceTag("listing", "div");
+    testCase.assertReplaceTag("dt", "span");
+    testCase.assertReplaceTag("datalist", "span");
+    testCase.assertReplaceTag("samp", "span");
+    testCase.assertReplaceTag("article", "div");
+    testCase.assertReplaceTag("menu", "ul");
+    testCase.assertReplaceTag("ins", "span");
+    testCase.assertReplaceTag("header", "div");
+    testCase.assertReplaceTag("optgroup", "span");
+    testCase.assertReplaceTag("ruby", "span");
+    testCase.assertReplaceTag("sub", "span");
+    testCase.assertReplaceTag("table", "p");
+    testCase.assertReplaceTag("tr", "p");
+    testCase.assertReplaceTag("td", "span");
+    testCase.assertReplaceTag("th", "span");
+    testCase.assertReplaceTag("h1", "h3");
+    testCase.assertReplaceTag("h2", "h3");
+    testCase.assertReplaceTag("h4", "h3");
+    testCase.assertReplaceTag("h5", "h3");
+    testCase.assertReplaceTag("h6", "h3");
+    testCase.assertReplaceTag("tbody", "span");
+
+    testCase.assertRemoveTag("applet");
+    testCase.assertRemoveTag("area");
+    testCase.assertRemoveTag("base");
+    testCase.assertRemoveTag("basefont");
+    testCase.assertRemoveTag("bgsound");
+    testCase.assertRemoveTag("command");
+    testCase.assertRemoveTag("canvas");
+    testCase.assertRemoveTag("col");
+    testCase.assertRemoveTag("colgroup");
+    testCase.assertRemoveTag("comment");
+    testCase.assertRemoveTag("strike");
+    testCase.assertRemoveTag("title");
+    //testCase.assertRemoveTag("iframe");
+    testCase.assertRemoveTag("br");
+    testCase.assertRemoveTag("noframes");
+    testCase.assertRemoveTag("head");
+    testCase.assertRemoveTag("object");
+    testCase.assertRemoveTag("track");
+    testCase.assertRemoveTag("wbr");
+    testCase.assertRemoveTag("noscript");
+    testCase.assertRemoveTag("svg");
+    testCase.assertRemoveTag("input");
+    testCase.assertRemoveTag("keygen");
+    testCase.assertRemoveTag("meta");
+    testCase.assertRemoveTag("isindex");
+    testCase.assertRemoveTag("video");
+    testCase.assertRemoveTag("spacer");
+    testCase.assertRemoveTag("source");
+    testCase.assertRemoveTag("frame");
+    testCase.assertRemoveTag("del");
+    testCase.assertRemoveTag("style");
+    testCase.assertRemoveTag("device");
+    testCase.assertRemoveTag("embed");
+    testCase.assertRemoveTag("noembed");
+    testCase.assertRemoveTag("xml");
+    testCase.assertRemoveTag("param");
+    testCase.assertRemoveTag("nextid");
+    testCase.assertRemoveTag("audio");
+    testCase.assertRemoveTag("link");
+    testCase.assertRemoveTag("script");
+    testCase.assertRemoveTag("frameset");
+
+    testCase.assertEmptyRule("b");
+    testCase.assertEmptyRule("code");
+    testCase.assertEmptyRule("cite");
+    testCase.assertEmptyRule("em");
+    testCase.assertEmptyRule("ul");
+    testCase.assertEmptyRule("li");
+    testCase.assertEmptyRule("i");
+    testCase.assertEmptyRule("strong");
+    testCase.assertEmptyRule("span");
+    testCase.assertEmptyRule("ol");
+    testCase.assertEmptyRule("pre");
+    //testCase.assertEmptyRule("p");
+    testCase.assertEmptyRule("h3");
+    testCase.assertEmptyRule("blockquote");
+
+    testCase.assertSetAttributes("a", {
+      "rel": "nofollow",
+      "target": "_blank"
+    });
+    testCase.assertAttributesValues("a", {
+      "href": "href"
+    });
+    testCase.assertAttributesValues("img", {
+      "width": "numbers",
+      "alt": "alt",
+      "src": "src",
+      "height": "numbers"
+    });
+    testCase.assertAttributesValues("q",  {
+      "cite": "url"
+    });
+
+    testCase.assertSetClass("big", "wysiwyg-font-size-larger");
+    testCase.assertSetClass("center", "wysiwyg-text-align-center");
+    testCase.assertSetClass("small", "wysiwyg-font-size-smaller");
+    testCase.assertSetClass("hr", "period-divider");
+    testCase.assertAddClass("caption", {
+      "align": "align_text"
+    });
+    testCase.assertAddClass("img", {
+      "align": "align_img"
+    });
+    testCase.assertAddClass("tfoot", {
+      "align": "align_text"
+    });
+    testCase.assertAddClass("font", {
+      "size": "size_font"
+    });
+    testCase.assertAddClass("thead", {
+      "align": "align_text"
+    });
+
+
+
+    testCase.assertNodeParserRule(tags, 'a', 'WWW.GOOGLE.COM', 'www.google.com');
+    testCase.assertNodeParserRule(tags, 'a', 'WWW . GOOGLE . COM', 'www.google.com');
+
+    testCase.assertNodeParserRule(tags, 'p', 'um texto de teste', 'um texto de teste.');
+    testCase.assertNodeParserRule(tags, 'p', 'um texto de teste!', 'um texto de teste!');
+    testCase.assertNodeParserRule(tags, 'p', '...um texto de teste!', '(...)um texto de teste!');
+
+    var denyRules = parseRules.deny;
+
+    testCase.assertDenyRule(denyRules[0], ['Um texto com  espaços extras']);
+    testCase.assertDenyRule(denyRules[1], ['Um texto com espaços antes de pontuação .']);
+    testCase.assertDenyRule(denyRules[2], ["Um texto com espaços no fim da linha \n"]);
+    testCase.assertDenyRule(denyRules[3], ["Um texto com muita exclamação !!!!!!!!"]);
+    testCase.assertDenyRule(denyRules[4], ["Um texto com muita pontuação ....."]);
+    testCase.assertDenyRule(denyRules[5], ["Um texto com muitos símbolos @@@@@@@@"]);
+
+
+    var fixRules = parseRules.fix;
+
+    testCase.assertFixRule(fixRules[0], [
+      {
+        text:'Olá,',
+        input:'e',
+        expec:'Olá, e'
+      },
+      {
+        text:'Olá',
+        input:',',
+        expec:'Olá, '
+      }
+    ]);
+
+    testCase.assertFixRule(fixRules[1], [
+      {
+        text:'http:,',
+        input:'/',
+        expec:'http:/'
+      },
+      {
+        text:'E foi assim:',
+        input:'J',
+        expec:'E foi assim: J'
+      },
+      {
+        text:'E foi assim',
+        input:':',
+        expec:'E foi assim: '
+      }
+    ]);
+
+    testCase.assertFixRule(fixRules[2], [
+      {
+        text:'Olá!',
+        input:'e',
+        expec:'Olá! E'
+      },
+      {
+        text:'Olá',
+        input:'!',
+        expec:'Olá! '
+      },
+    ]);
+
+    testCase.assertFixRule(fixRules[3], [
+      {
+        text:'Olá. ',
+        input:'e',
+        expec:'Olá. E'
+      },
+      {
+        text:'Olá! ',
+        input:'e',
+        expec:'Olá! E'
+      },
+      {
+        text:'Olá? ',
+        input:'e',
+        expec:'Olá? E'
+      },
+    ]);
+
+    var parserRules = parseRules.parser;
+
+    var text = " este é um texto com muitos erros.Alguns bem grosseiros .Outros , nem tanto,nem tão pouco . Espaços    repetidos !!!! acredita???? Símbolos @@@@ repetidos .... muitos deles várias vezes ";
+    var expec = "Este é um texto com muitos erros. Alguns bem grosseiros. Outros, nem tanto, nem tão pouco. Espaços repetidos! Acredita? Símbolos @ repetidos... Muitos deles várias vezes"
+
+    testCase.assertParserRules(parserRules, text, expec);
+
+  });
+});
